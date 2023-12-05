0c40c7f1d32191274f21c29832164abb9fa53fca
userA <geniusssbg@gmail.com>
2023-12-06 02:53:15 +0800

    发布 Version 1.0.4版本

--- assistant-boot-start/src/main/java/io/github/config/AppConfig.java
+++ assistant-boot-start/src/main/java/io/github/config/AppConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@ComponentScan("io.github")
+@ComponentScan("io.github.*")
 public class AppConfig {
+
+    public AppConfig() {
+    }
 }
