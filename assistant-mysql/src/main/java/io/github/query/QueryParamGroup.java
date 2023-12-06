309f15efb62e1735dbfdc497479a0c2cb1135d00
userA <geniusssbg@gmail.com>
2023-12-07 01:09:32 +0800

    发布 v1.0.7 版本 修改QueryParamGroup

--- assistant-mysql/src/main/java/io/github/query/QueryParamGroup.java
+++ assistant-mysql/src/main/java/io/github/query/QueryParamGroup.java
@@ -1,20 +1,24 @@
 package io.github.query;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 @Component
+@Data
+@ConfigurationProperties(prefix = "assistant.mysql")
 public class QueryParamGroup {
 
+    private Map<String, List<String>> queryGroup = new HashMap<>();
 
-    @Value("#{${assistant.mysql.query:{}}}")
-    private Map<String,String> query;
+    public List<String> getQueryParams(String queryType){
 
-    public String getQueryParams(String queryType){
-
-        return Optional.ofNullable(query.get(queryType)).orElse("");
+        return Optional.ofNullable(queryGroup.get(queryType)).orElse(List.of());
     }
 }
